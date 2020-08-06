023ccfc530adabf28c18bdcbf9a42a3b94ecb347
Luiz Augusto Tavares da Silveira <luiz.silveira@me.com.br>
2020-08-05 21:27:11 -0300

    Cadastro de usuários

---
+++ aulas/server/src/utils/convertHourToMinute.ts
@@ -0,0 +1,6 @@
+export default function convertHourToMinute(time: string) {
+    const [hour, minutes] = time.split(':').map(Number);
+    const timeinMinutes = (hour * 60) + minutes;
+    
+    return timeinMinutes;
+}
